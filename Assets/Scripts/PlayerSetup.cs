bd1be3f0b3d945071587706171995498ff3ea876
Ethan Frazier <zenedf@protonmail.com>
2020-03-26 16:40:51 -0400

    two different machines can play the game with issues

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField] Behaviour[] componentsToDisable;
+
+    void Start()
+    {
+        // check to see if we're on the network.
+        if (!isLocalPlayer)
+        {
+            // if this object isn't controlled by the system, disable all the components.
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+    }
+}
